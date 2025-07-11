8df82170a5ce0f3e5cf7e3b84a3709e560383f1c
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-07-11 21:16:11 +0600

    Unnecessary File removed

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,16 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
-
 Route::get('/chat/{user}', [ChatController::class, 'viewPage'])->middleware(['auth', 'verified'])->name('chat');
-Route::resource('messages/{user}',ChatController::class, ['only' => ['index', 'store']])->middleware(['auth']);
+Route::resource('messages/{user}', ChatController::class, ['only' => ['index', 'store']])->middleware(['auth']);
 
 require __DIR__.'/auth.php';
